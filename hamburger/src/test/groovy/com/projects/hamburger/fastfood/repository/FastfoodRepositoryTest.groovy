e190ba86d356f4ddadce97870a1fa6fa8d7e7c85
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-17 00:14:34 +0900

    Implemented repository save test

--- hamburger/src/test/groovy/com/projects/hamburger/fastfood/repository/FastfoodRepositoryTest.groovy
+++ hamburger/src/test/groovy/com/projects/hamburger/fastfood/repository/FastfoodRepositoryTest.groovy
@@ -1,15 +1,34 @@
 package com.projects.hamburger.fastfood.repository
 
+import com.projects.AbstractIntegrationContainerBaseTest
+import com.projects.hamburger.fastfood.entity.Fastfood
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import spock.lang.Specification
 
-@SpringBootTest
-class FastfoodRepositoryTest extends Specification {
+class FastfoodRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private FastfoodRepository fastfoodRepository;
 
-    def "test"(){
-        
+    def "FastfoodRepository Save"(){
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def fastfood = Fastfood.builder()
+                .storeAddress(address)
+                .storeName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def result = fastfoodRepository.save(fastfood)
+
+        then:
+        result.getStoreAddress() == address
+        result.getStoreName() == name
+        result.getLatitude() == latitude
+        result.getLongitude() == longitude
     }
 }
