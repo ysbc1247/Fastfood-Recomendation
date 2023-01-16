40f5dbf067960ddc9a5e4ea1d58034ff156c2f10
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-16 16:21:48 +0900

    Implemented DocumentDto for getting API data

---
+++ hamburger/src/main/java/com/projects/hamburger/api/dto/DocumentDto.java
@@ -0,0 +1,21 @@
+package com.projects.hamburger.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DocumentDto {
+    @JsonProperty("address_name")
+    private String AddressName;
+
+    @JsonProperty("y")
+    private Double latitude;
+
+    @JsonProperty("x")
+    private Double longitude;
+}
