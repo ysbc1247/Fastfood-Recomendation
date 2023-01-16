5ee6e8b78763483240c5c792b4c8c2b68949682c
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-16 18:08:15 +0900

    Implemented FastfoodRepositoryTest

---
+++ hamburger/src/test/groovy/com/projects/hamburger/fastfood/repository/FastfoodRepositoryTest.groovy
@@ -0,0 +1,15 @@
+package com.projects.hamburger.fastfood.repository
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class FastfoodRepositoryTest extends Specification {
+    @Autowired
+    private FastfoodRepository fastfoodRepository;
+
+    def "test"(){
+        
+    }
+}
