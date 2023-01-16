4480e1b5242c730fc818da0033a1b0b49deff51b
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-16 17:31:17 +0900

    Made groovy Test Class for URI decoding - deprecated function?

---
+++ hamburger/src/test/groovy/com/projects/hamburger/api/service/KakaoURIBuilderServiceTest.groovy
@@ -0,0 +1,26 @@
+package com.projects.hamburger.api.service
+
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+
+class KakaoURIBuilderServiceTest extends Specification {
+    KakaoURIBuilderService kakaoURIBuilderService;
+
+    def setup(){
+        kakaoURIBuilderService = new KakaoURIBuilderService();
+    }
+
+    def "buildURIByAddressSearch"(){
+        given:
+        String address = "서울 성북구"
+        def charset = StandardCharsets.UTF_8
+
+        when:
+        def uri = kakaoURIBuilderService.buildUriByAddressSearch(address)
+        def decodedResult = URLDecoder.decode(uri.toString(), charset)
+
+        then:
+        decodedResult == "https://dapi.kakao.com/v2/local/search/address.json?query=서울 성북구"
+    }
+}
