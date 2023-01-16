adcd32d941b4c84083afbab18eb3c80462743ec0
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-16 16:22:13 +0900

    Implemented MetaDto

---
+++ hamburger/src/main/java/com/projects/hamburger/api/dto/MetaDto.java
@@ -0,0 +1,18 @@
+package com.projects.hamburger.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MetaDto {
+
+    @JsonProperty("total_count")
+    private Long totalCount;
+
+
+}
