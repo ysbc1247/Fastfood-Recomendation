66b270fbaa7de4e401f3ecb3b18df00ff31da78d
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-17 18:33:13 +0900

    Implemented setup module to rebase database after each test

--- hamburger/src/test/groovy/com/projects/hamburger/fastfood/repository/FastfoodRepositoryTest.groovy
+++ hamburger/src/test/groovy/com/projects/hamburger/fastfood/repository/FastfoodRepositoryTest.groovy
@@ -8,6 +8,10 @@ class FastfoodRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private FastfoodRepository fastfoodRepository;
 
+    def setup(){
+        fastfoodRepository.deleteAll()
+    }
+
     def "FastfoodRepository Save"(){
         given:
         String address = "서울 특별시 성북구 종암동"
@@ -30,5 +34,26 @@ class FastfoodRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getStoreName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "FastfoodRepository SaveAll"(){
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+        def fastfood = Fastfood.builder()
+                .storeAddress(address)
+                .storeName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        fastfoodRepository.saveAll(Arrays.asList(fastfood))
+        def result = fastfoodRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
