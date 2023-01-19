27cb946d872a5e669e267d39ba84bc7b7ce9319d
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-19 17:41:00 +0900

    Refactored main package name

--- hamburger/src/main/java/com/projects/Fastfood/fastfood/repository/FastfoodRepository.java
+++ hamburger/src/main/java/com/projects/Fastfood/fastfood/repository/FastfoodRepository.java
@@ -1,6 +1,6 @@
-package com.projects.hamburger.fastfood.repository;
+package com.projects.Fastfood.fastfood.repository;
 
-import com.projects.hamburger.fastfood.entity.Fastfood;
+import com.projects.Fastfood.fastfood.entity.Fastfood;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FastfoodRepository extends JpaRepository<Fastfood, Long> {
