c54ace832e0ea8daecab75f4c84f26d4808d67f7
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-19 17:41:08 +0900

    Refactored main package name

--- hamburger/src/main/java/com/projects/Fastfood/FastfoodApplication.java
+++ hamburger/src/main/java/com/projects/Fastfood/FastfoodApplication.java
@@ -1,8 +1,10 @@
-package com.projects.hamburger;
+package com.projects.Fastfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FastfoodApplication {
 
