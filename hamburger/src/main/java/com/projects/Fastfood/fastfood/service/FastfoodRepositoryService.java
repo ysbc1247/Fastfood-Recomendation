23b5548dc5dd3b28c0948a390207564652377b9f
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-19 22:23:06 +0900

    Implemented findAll function in repositoryservice

--- hamburger/src/main/java/com/projects/Fastfood/fastfood/service/FastfoodRepositoryService.java
+++ hamburger/src/main/java/com/projects/Fastfood/fastfood/service/FastfoodRepositoryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -37,6 +38,11 @@ public class FastfoodRepositoryService {
         }
 
         entity.changeStoreAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Fastfood> findAll(){
+        return fastfoodRepository.findAll();
     }
 
 }
