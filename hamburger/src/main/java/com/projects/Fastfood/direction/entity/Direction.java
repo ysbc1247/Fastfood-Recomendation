c19ec32410959fd1136f2c7153d441a3be834ec5
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-20 11:44:35 +0900

    Implemented Direction Entity

---
+++ hamburger/src/main/java/com/projects/Fastfood/direction/entity/Direction.java
@@ -0,0 +1,34 @@
+package com.projects.Fastfood.direction.entity;
+
+import com.projects.Fastfood.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "direction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Direction extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    private String targetStoreName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    private double distance;
+}
