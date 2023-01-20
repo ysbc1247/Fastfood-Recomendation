5db6d71f838c675a1bfef93042dbf57ae4839a99
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-20 13:13:14 +0900

    Created DirectionRepository class

---
+++ hamburger/src/main/java/com/projects/Fastfood/direction/repository/DirectionRepository.java
@@ -0,0 +1,8 @@
+package com.projects.Fastfood.direction.repository;
+
+import com.projects.Fastfood.direction.entity.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectionRepository extends JpaRepository<Direction, Long> {
+
+}
