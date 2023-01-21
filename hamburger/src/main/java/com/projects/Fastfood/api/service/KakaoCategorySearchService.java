b3cc869cd4ff4a9b4683c2637d884f96ec783406
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-21 22:45:04 +0900

    Implemented KakaoCategorySearchService

---
+++ hamburger/src/main/java/com/projects/Fastfood/api/service/KakaoCategorySearchService.java
@@ -0,0 +1,42 @@
+package com.projects.Fastfood.api.service;
+
+
+import com.projects.Fastfood.api.dto.KakaoAPIResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+
+public class KakaoCategorySearchService {
+
+    private final KakaoURIBuilderService kakaoURIBuilderService;
+
+    private final RestTemplate restTemplate;
+
+    private static final String PHARMACY_CATEGORY = "PM9";
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+    public KakaoAPIResponseDto requestPharmacyCategorySearch(double latitude, double longitude, double radius) {
+
+        URI uri = kakaoURIBuilderService.buildUriByCategorySearch(latitude, longitude, radius, PHARMACY_CATEGORY);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK "+ kakaoRestApiKey);
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoAPIResponseDto.class).getBody();
+    }
+
+}
