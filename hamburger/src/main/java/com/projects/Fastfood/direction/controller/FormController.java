23a949510aab41814697b4d7aac533f6865e49ad
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-21 23:07:36 +0900

    Made controller for mapping

---
+++ hamburger/src/main/java/com/projects/Fastfood/direction/controller/FormController.java
@@ -0,0 +1,18 @@
+package com.projects.Fastfood.direction.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+
+public class FormController {
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+
+}
