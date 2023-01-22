dcdfd5a36ae7eedbc52c8a0e7e0bf254cf57146a
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-22 21:54:38 +0900

    Refactored Service of directionurl and roadviewurl

--- hamburger/src/main/java/com/projects/Fastfood/fastfood/service/FastfoodRecommendationService.java
+++ hamburger/src/main/java/com/projects/Fastfood/fastfood/service/FastfoodRecommendationService.java
@@ -5,11 +5,14 @@ import com.projects.Fastfood.api.dto.KakaoAPIResponseDto;
 import com.projects.Fastfood.api.service.KakaoAddressSearchService;
 import com.projects.Fastfood.direction.dto.OutputDto;
 import com.projects.Fastfood.direction.entity.Direction;
+import com.projects.Fastfood.direction.service.Base62Service;
 import com.projects.Fastfood.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -25,6 +28,11 @@ public class FastfoodRecommendationService {
 
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    private final Base62Service base62Service;
+
+    @Value("${fastfood.recommendation.base.url}")
+    private String baseUrl;
 
     public List<OutputDto> recommendFastfoodList(String address) {
         KakaoAPIResponseDto kakaoAPIResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
@@ -43,11 +51,13 @@ public class FastfoodRecommendationService {
     }
 
     private OutputDto convertToOutputDto(Direction direction) {
+
+
         return OutputDto.builder()
                 .storeName(direction.getTargetStoreName())
                 .storeAddress(direction.getTargetAddress())
-                .directionUrl("todo")
-                .roadViewUrl("todo")
+                .directionUrl(baseUrl + base62Service.encodeDirectionId(direction.getId()))
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
                 .build();
     }
