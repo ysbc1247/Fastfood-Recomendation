b9bbacdac338b074f3844bc4b3cb7a1b01bacd80
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-22 21:07:35 +0900

    Implemented /search controller

--- hamburger/src/main/java/com/projects/Fastfood/direction/controller/FormController.java
+++ hamburger/src/main/java/com/projects/Fastfood/direction/controller/FormController.java
@@ -1,18 +1,36 @@
 package com.projects.Fastfood.direction.controller;
 
 
+import com.projects.Fastfood.direction.dto.InputDto;
+import com.projects.Fastfood.fastfood.service.FastfoodRecommendationService;
+import com.projects.Fastfood.fastfood.service.FastfoodSearchService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@Slf4j
+@RequiredArgsConstructor
 @Controller
 
 public class FormController {
 
+    private final FastfoodRecommendationService fastfoodRecommendationService;
+
     @GetMapping("/")
     public String main(){
         return "main";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView postDirection(@ModelAttribute InputDto inputDto) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("output");
+        modelAndView.addObject("outputFormList",
+                fastfoodRecommendationService.recommendFastfoodList(inputDto.getAddress()));
+        return modelAndView;
     }
 
 }
