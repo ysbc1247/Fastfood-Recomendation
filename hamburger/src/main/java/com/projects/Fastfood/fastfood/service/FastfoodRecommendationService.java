89025173e96c2e7f6389b02e8955aa0a06c726b9
ysbc1247 <ystc1247@khu.ac.kr>
2023-01-22 21:08:22 +0900

    Implemented OutputDto method for conversion

--- hamburger/src/main/java/com/projects/Fastfood/fastfood/service/FastfoodRecommendationService.java
+++ hamburger/src/main/java/com/projects/Fastfood/fastfood/service/FastfoodRecommendationService.java
@@ -3,6 +3,7 @@ package com.projects.Fastfood.fastfood.service;
 import com.projects.Fastfood.api.dto.DocumentDto;
 import com.projects.Fastfood.api.dto.KakaoAPIResponseDto;
 import com.projects.Fastfood.api.service.KakaoAddressSearchService;
+import com.projects.Fastfood.direction.dto.OutputDto;
 import com.projects.Fastfood.direction.entity.Direction;
 import com.projects.Fastfood.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -23,16 +26,29 @@ public class FastfoodRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendFastfoodList(String address){
+    public List<OutputDto> recommendFastfoodList(String address) {
         KakaoAPIResponseDto kakaoAPIResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
-        if(Objects.isNull(kakaoAPIResponseDto) || CollectionUtils.isEmpty(kakaoAPIResponseDto.getDocumentList())){
+        if (Objects.isNull(kakaoAPIResponseDto) || CollectionUtils.isEmpty(kakaoAPIResponseDto.getDocumentList())) {
             log.error("Input address {} is not valid/null", address);
-            return;
+            return Collections.emptyList();
         }
         DocumentDto documentDto = kakaoAPIResponseDto.getDocumentList().get(0);
         List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
 
-        directionService.saveAll(directionList);
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
+
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .storeName(direction.getTargetStoreName())
+                .storeAddress(direction.getTargetAddress())
+                .directionUrl("todo")
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
